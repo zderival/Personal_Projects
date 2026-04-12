272d5a2334d07a0d0e6372deb3eb24be69cffe9e
Zachary Derival <zderival@gmail.com>
2026-04-12 00:19:40 -0400

    Refactor Profile class and add email/password change methods

--- WhatsNew - Python/Profile.py
+++ WhatsNew - Python/Profile.py
@@ -1,73 +1,76 @@
-#Every profile will have the NewsManager Class
-
-import random
 import os
 import shutil
 from NewsManagment import NewsManager
-
-
+from Email_Maintance import send_verification_code
 class Profile:
-    def __init__(self, username, email, password, id, profile_pic,display_color = None, newUser = True):
-        self.username = username
-        self.email = email
-        self.password = password
+    def __init__(self, id, profile_pic = None,display_color = None):
         self.id = id
-        self.newUser = newUser
         self.display_color = display_color
         self.profile_pic = profile_pic
         self.article_preferences = []
         self.saved_articles = []
         self.new_manager = NewsManager()
-    def change_email(self):
-        class InvalidEmailException(Exception):
-            pass
-        new_email = input("Enter new email: ")
-        verify_code = f"{random.randint(0,99999):05d}"
-        # here is where the code is sent to the email
-        attempts = 5
-        while attempts != 0:
-            try:
-                print("A code was sent to your email for verification.")
-                verify_email = input("Please input the code: ")
-                if verify_code != verify_email:
-                    raise InvalidEmailException("Wrong code, please try again")
-                # Option to resend the code here
-                else:
-                    self.email = new_email
-            except InvalidEmailException as e:
-                print(e)
-        print("Too many failed attempts, can no longer change email for 14 days.")
-        # Find a way to make the function inaccessible
 
-    def change_password(self):
+    def change_email(self,cursor,conn):
+        while True:
+            new_email = input("Enter new email: ").strip()
+            if "@" not in new_email or "." not in new_email:
+                print("Invalid email")
+                continue
+            else:
+                break
+        code = send_verification_code(new_email)
+        if not code:
+            print("Failed to send verification email.")
+            return
+        while True:
+            verification = input("Enter your verification code: ")
+            if verification != code:
+                print("Incorrect code, please try again")
+            else:
+                break
+        sql = """ UPDATE "user" SET email = %s WHERE id = %s; """
+        cursor.execute(sql,(new_email,self.id))
+        conn.commit()
+        print("Your email has changed")
+
+    def change_password(self,cursor,conn):
         class InvalidPasswordChange(Exception):
             pass
-        email = input("Enter email: ")
-        verify_code = str(random.randint(00000, 99999))
-        # send code to User
-        attempts = 5
-        while attempts != 0:
-            try:
+        sql = """ SELECT email FROM "user" WHERE id = %s; """
+        cursor.execute(sql,(self.id,))
+        result = cursor.fetchone()
+        user_db_email = result["email"]
+        while True:
+            email = input("Enter email: ")
+            if user_db_email != email:
+                print("Incorrect email please try again")
+                continue
+            else:
+                code = send_verification_code(email)
                 print("A code was sent to your email for verification.")
-                verify_email = input("Please input the code: ")
-                if verify_code != verify_email:
-                    raise InvalidPasswordChange("Wrong code, please try again")
-                # Option to resend the code here
+                verify_code = input("Please input the code: ")
+                if verify_code != code:
+                    print("Incorrect code, please try again")
                 else:
-                    new_password = input("Enter new password: ")
-                    if len(new_password) < 8 or len(new_password) > 32:
-                        raise InvalidPasswordChange("Password must be 8-32 characters")
-                    elif not any(new_password.upper() for char in new_password) or not any(new_password.lower() for char in new_password):
-                        raise InvalidPasswordChange("Must contain at least must 1 upper case and 1 lowercase letter")
-                    elif not any(char.isdigit() for char in new_password):
-                        raise InvalidPasswordChange("Must have at least one digit in your password")
-                    else:
-                        self.password = new_password
+                    break
+        while True:
+            try:
+                new_password = input("Enter new password: ")
+                if len(new_password) < 8 or len(new_password) > 32:
+                    raise InvalidPasswordChange("Password must be 8-32 characters")
+                elif not any(char.isupper() for char in new_password) or not any(char.islower() for char in new_password):
+                    raise InvalidPasswordChange("Must contain at least must 1 upper case and 1 lowercase letter")
+                elif not any(char.isdigit() for char in new_password):
+                    raise InvalidPasswordChange("Must have at least one digit in your password")
+                else:
+                    sql = """UPDATE "user" SET password = %s WHERE id = %s;"""
+                    cursor.execute(sql,(new_password,self.id))
+                    conn.commit()
+                    print("Your password has changed.")
+                    break
             except InvalidPasswordChange as e:
                 print(e)
-        print("Too many failed attempts, can no longer change password for 14 days.")
-        # Find a way to make the function inaccessible
-
     def profile_pic(self):
         profile_pic_folder = "profile pics"
         if not os.path.exists(profile_pic_folder):
@@ -82,11 +85,8 @@ class Profile:
         shutil.copy(user_pic,destination_path)
         self.profile_pic = user_pic
 
-    def change_display_color(self):
+    def change_username(self):
         pass
-    def change_article_preference(self):
-        print("Available topics: tech fashion sports music politics health science ")
-        select_topics = input("What topics would you like to see? ").lower()
-        self.article_preferences = select_topics.split(" ")
-        # Change the user's article preferences
-        # Change the user's article preferences
+
+    def delete_profile(self):
+        pass
